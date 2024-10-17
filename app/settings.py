81f176e8f4fe8a1d6d298d4341e64f70d93efbb4
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-17 19:00:33 +0000

    add shipment table

--- app/settings.py
+++ app/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        
     }
 }
 
+DATABASES['default']= dj_database_url.config(default=REMOVED)
+db_from_env = dj_database_url.config(conn_max_age = 600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- dash_board/models.py
+++ dash_board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Shipment(models.Model):
+	shipmentid = models.CharField(max_length= 20)
+	consignee = models.CharField(max_length= 200)
+	ata = models.CharField(max_length= 50)
+	cargoavailable = models.CharField(max_length= 50)
+	datecleared = models.CharField(max_length= 50)
+	actualdelivery = models.CharField(max_length= 50)
+	cont = models.CharField(max_length= 10)
+	twentyft = models.CharField(max_length= 10)
+	fortyft = models.CharField(max_length= 10)
+	uw = models.CharField(max_length= 10)
+	weight = models.CharField(max_length= 10)
+
+	def __str__(self):
+		return self.name 
