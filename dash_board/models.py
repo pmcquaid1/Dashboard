1db1c006756ee425cc11cc262b46c5bc3ffe7f67
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-21 15:05:59 +0000

    add back line 12 from pathlib import Path

--- dash_board/models.py
+++ dash_board/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Shipment(models.Model):
-	shipmentid = models.CharField(max_length= 20)
+	shipment_id = models.CharField(max_length= 20)
 	consignee = models.CharField(max_length= 200)
 	ata = models.CharField(max_length= 50)
-	cargoavailable = models.CharField(max_length= 50)
-	datecleared = models.CharField(max_length= 50)
-	actualdelivery = models.CharField(max_length= 50)
+	cargo_available = models.CharField(max_length= 50)
+	date_cleared = models.CharField(max_length= 50)
+	actual_delivery = models.CharField(max_length= 50)
 	cont = models.CharField(max_length= 10)
-	twentyft = models.CharField(max_length= 10)
-	fortyft = models.CharField(max_length= 10)
+	twenty_ft = models.CharField(max_length= 10)
+	forty_ft = models.CharField(max_length= 10)
 	uw = models.CharField(max_length= 10)
 	weight = models.CharField(max_length= 10)
 
