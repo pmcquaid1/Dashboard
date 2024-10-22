24997336983c84dc78a734dfcf32431561feac35
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-22 14:26:52 +0000

    deleted max length on int fields

--- dash_board/models.py
+++ dash_board/models.py
@@ -3,15 +3,15 @@ from django.db import models
 class Shipment(models.Model):
 	shipment_id = models.CharField(max_length= 15)
 	consignee = models.CharField(max_length= 25)
-	ata = models.DateField(max_length= 50)
-	cargo_available = models.DateField(max_length= 50)
-	date_cleared = models.DateField(max_length= 50)
-	actual_delivery = models.DateField(max_length= 50)
+	ata = models.DateField()
+	cargo_available = models.DateField()
+	date_cleared = models.DateField()
+	actual_delivery = models.DateField()
 	cont = models.CharField(max_length= 10)
-	twenty_ft = models.IntegerField(max_length= 10)
-	forty_ft = models.IntegerField(max_length= 10)
+	twenty_ft = models.IntegerField()
+	forty_ft = models.IntegerField()
 	uw = models.CharField(max_length= 10)
-	weight = models.IntegerField(max_length= 10)
+	weight = models.IntegerField()
 
 	def __str__(self): 
 		return self.shipment_id
