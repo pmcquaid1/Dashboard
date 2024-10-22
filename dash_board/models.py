7e3eb3034f83d8b888bde509e607bb98fb02ee96
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-22 16:43:11 +0000

    revert back to CharField

--- dash_board/models.py
+++ dash_board/models.py
@@ -3,15 +3,15 @@ from django.db import models
 class Shipment(models.Model):
 	shipment_id = models.CharField(max_length= 15)
 	consignee = models.CharField(max_length= 25)
-	ata = models.DateField()
-	cargo_available = models.DateField()
-	date_cleared = models.DateField()
-	actual_delivery = models.DateField()
+	ata = models.CharField(max_length= 25)
+	cargo_available = models.CharField(max_length= 25)
+	date_cleared = models.CharField(max_length= 25)
+	actual_delivery = models.CharField(max_length= 25)
 	cont = models.CharField(max_length= 10)
-	twenty_ft = models.IntegerField()
-	forty_ft = models.IntegerField()
+	twenty_ft = models.CharField(max_length= 25)
+	forty_ft = models.CharField(max_length= 25)
 	uw = models.CharField(max_length= 10)
-	weight = models.IntegerField()
+	weight = models.CharField(max_length= 25)
 
 	def __str__(self): 
 		return self.shipment_id
