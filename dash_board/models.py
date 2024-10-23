a54cdaf6497dacbb32790ff4818991b597d7ef0d
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-23 09:18:20 +0000

    updated all data fields

--- dash_board/models.py
+++ dash_board/models.py
@@ -4,14 +4,14 @@ class Shipment(models.Model):
 	shipment_id = models.CharField(max_length= 15)
 	consignee = models.CharField(max_length= 25)
 	ata = models.DateField()
-	cargo_available = models.CharField(max_length= 25)
-	date_cleared = models.CharField(max_length= 25)
-	actual_delivery = models.CharField(max_length= 25)
-	cont = models.CharField(max_length= 10)
-	twenty_ft = models.CharField(max_length= 25)
-	forty_ft = models.CharField(max_length= 25)
-	uw = models.CharField(max_length= 10)
-	weight = models.CharField(max_length= 25)
+	cargo_available = models.DateField()
+	date_cleared = models.DateField()
+	actual_delivery = models.DateField()
+	cont = models.CharField(max_length= 5)
+	twenty_ft = models.IntegerField(max_length= 10)
+	forty_ft = models.IntegerField(max_length= 10)
+	uw = models.CharField(max_length= 5)
+	weight = models.IntegerField(max_length= 10)
 
 	def __str__(self): 
 		return self.shipment_id
