5f4f9637a150593ec9f6d7baeecbea3d20482879
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-24 18:07:45 +0000

    adjust attibutes to add_shipment....not shipments

--- dash_board/urls.py
+++ dash_board/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('add_shipment', views.add_shipment, name="Shipment"),
     path('edit/<list_id>', views.edit, name="edit"),
-   
-]
+ 
+]  
