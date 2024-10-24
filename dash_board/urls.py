23a8eb9a37b85e3f2ebae2405de7a5a15ba52172
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-24 20:00:50 +0000

    fixed reverse error by ensuring url.py line name= x is the same as the href in nav link of base file

--- dash_board/urls.py
+++ dash_board/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('add_shipment', views.add_shipment, name="Shipment"),
+    path('add_shipment', views.add_shipment, name="add_shipment"),
     path('edit/<list_id>', views.edit, name="edit"),
  
 ]  
