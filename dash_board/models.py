f9acc9cdbfc7cb37401da4c09249d71c1722756e
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-27 11:09:10 +0000

    trying to display db info

--- dash_board/models.py
+++ dash_board/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Shipment(models.Model):
-	shipment_id = models.CharField(max_length= 15)
-	consignee = models.CharField(max_length= 25)
-	ata = models.DateField()
-	cargo_available = models.DateField()
-	date_cleared = models.DateField()
-	actual_delivery = models.DateField()
-	cont = models.CharField(max_length= 10)
-	twenty_ft = models.CharField(max_length= 20)
-	forty_ft = models.CharField(max_length= 20)
-	uw = models.CharField(max_length= 10)
-	weight = models.CharField(max_length= 15)
+	shipment_id = models.CharField(max_length= 50)
+	consignee = models.CharField(max_length= 50)
+	ata = models.CharField(max_length= 50)
+	cargo_available = models.CharField(max_length= 50)
+	date_cleared = models.CharField(max_length= 50)
+	actual_delivery = models.CharField(max_length= 50)
+	cont = models.CharField(max_length= 50)
+	twenty_ft = models.CharField(max_length= 50)
+	forty_ft = models.CharField(max_length= 50)
+	uw = models.CharField(max_length= 50)
+	weight = models.CharField(max_length= 50)
 
 	def __str__(self): 
 		return self.shipment_id
--- dash_board/views.py
+++ dash_board/views.py
@@ -12,7 +12,7 @@ def add_shipment(request):
 	if request.method =='POST':
 		form = ShipmentForm(request.POST or None)
 		if form.is_valid():
-			form.save()
+			form.save('home')
 			messages.success(request, ('Shipment has been added'))
 			return redirect('home')
 		else:
@@ -38,12 +38,3 @@ def edit(request, list_id):
 		get_shipment = Shipment.objects.get(pk=list_id)
 		return render(request, 'edit.html', {'get_shipment': get_shipment})
 
-def delete(request, list_id):
-	if request.method =='POST':
-		current_shipment = Shipment.objects.get(pk=list_id)
-		current_shipment.delete()
-		message.success(request, ('Shipment deleted'))
-		return redirect ('home')
-	else:
-		message.success(request, ('Cannot delete from Page'))
-		return redirect ('home')
