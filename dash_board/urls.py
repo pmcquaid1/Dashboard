16adacfbf45150247df59fad28f0d2b768244947
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-28 15:36:21 +0000

    attempt 1 to move shipment page data to newhome page

--- dash_board/urls.py
+++ dash_board/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.newhome, name="newhome"),
+    path('newhome', views.newhome, name="newhome"),
     path('add_shipment', views.add_shipment, name="add_shipment"),
     path('edit/<list_id>', views.edit, name="edit"),
     path('delete/<list_id>', views.delete, name= "delete"),
