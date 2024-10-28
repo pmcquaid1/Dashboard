ff79f6a160e62663ff60d2ff61ec4ac06eefccee
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-28 18:31:39 +0000

    reentered urls.py path for shipments

--- dash_board/urls.py
+++ dash_board/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('newhome', views.newhome, name="newhome"),
+    path('shipments', views.shipments, name="shipments"),
     path('add_shipment', views.add_shipment, name="add_shipment"),
     path('edit/<list_id>', views.edit, name="edit"),
     path('delete/<list_id>', views.delete, name= "delete"),
