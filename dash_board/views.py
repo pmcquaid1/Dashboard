206c745f74b4390450af6e705e7a0e9a4e85d315
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-10-28 18:09:15 +0000

    changed all instances of newhome to shipments

--- dash_board/views.py
+++ dash_board/views.py
@@ -4,12 +4,14 @@ from .forms import ShipmentForm
 from django.contrib import messages
 
 
-def newhome(request):
-	return render(request, 'newhome.html', {})
-
 def home(request):
+	return render(request, 'home.html', {})
+	
+
+def shipments(request):
 	all_shipments = Shipment.objects.all
-	return render(request, 'home.html', {'all_shipments':all_shipments})
+	return render(request, 'shipments.html', {'all_shipments':all_shipments})
+
 
 def add_shipment(request):
 	if request.method =='POST':
@@ -17,7 +19,7 @@ def add_shipment(request):
 		if form.is_valid():
 			form.save()
 			messages.success(request, ('Shipment has been added'))
-			return redirect('home')
+			return redirect('shipments')
 		else:
 			messages.success(request, ('Error'))
 			return render(request, 'add_shipment.html', {})
@@ -32,7 +34,7 @@ def edit(request, list_id):
 		if form.is_valid():
 			form.save()
 			messages.success(request, ('Shipment has been edited'))
-			return redirect('home')
+			return redirect('shipments')
 		else:
 			messages.success(request, ('Error'))
 			return render(request, 'edit.html', {})
@@ -46,9 +48,9 @@ def delete(request, list_id):
 		current_shipment = Shipment.objects.get(pk=list_id)
 		current_shipment.delete()
 		messages.success(request, ('Shipment deleted'))
-		return redirect ('home')
+		return redirect ('shipments')
 	else:
 		messages.success(request, ('Cannot delete from Page'))
-		return redirect ('home')
+		return redirect ('shipments')
 
 
