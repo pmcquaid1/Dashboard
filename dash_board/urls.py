8ca2dc0d25410ad037f2e633f027dca240f0ada8
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-11-01 10:17:32 +0000

    creating form and data entry within template

--- dash_board/urls.py
+++ dash_board/urls.py
@@ -4,15 +4,16 @@ from . import views
 app_name= 'website'
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('shipments', views.shipments, name="shipments"),
-    path('add_shipment', views.add_shipment, name="add_shipment"),
-    path('edit/<list_id>', views.edit, name="edit"),
-    path('delete/<list_id>', views.delete, name= "delete"),
+    path('', views.home2, name="home2"),
     path('samples', views.samples, name= "samples"),
     path('ops', views.ops, name= "ops"),
     path('finance', views.finance, name= "finance"),
     path('qhse', views.qhse, name= "qhse"),
+    path('upload', views.upload, name= "upload"),
     path('hr_support', views.hr_support, name= "hr_support"),
+    path('shipments', views.shipments, name="shipments"),
+    path('upload/add_shipment', views.add_shipment, name="add_shipment"),
+    path('edit/<list_id>', views.edit, name="edit"),
+    path('delete/<list_id>', views.delete, name= "delete"),
     
 ]  
