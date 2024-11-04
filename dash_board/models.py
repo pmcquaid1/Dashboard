b1f9292f5ba53e471827d203ed33af1ac95f8b7d
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-11-04 19:50:37 +0000

    changed date fields in shipment model from CharField to DateField

--- dash_board/models.py
+++ dash_board/models.py
@@ -1,27 +1,20 @@
 from django.db import models
 
-class Category(models.Model):
-	name=models.CharField(max_length= 50, unique=True)
-	
-	class Meta:
-		verbose_name_plural= 'Categories'
-
-def __str__(self): 
-		return self.name
 
 class Shipment(models.Model):
 	shipment_id = models.CharField(max_length= 50)
 	consignee = models.CharField(max_length= 50)
-	ata = models.DateField(max_length= 15)
-	cargo_available = models.DateField(max_length= 20)
-	date_cleared = models.DateField(max_length= 20)
-	actual_delivery = models.DateField(max_length= 20)
+	ata = models.DateField()
+	cargo_available = models.DateField()
+	date_cleared = models.DateField()
+	actual_delivery = models.DateField()
 	cont = models.CharField(max_length= 50)
 	twenty_ft = models.CharField(max_length= 50)
 	forty_ft = models.CharField(max_length= 50)
 	uw = models.CharField(max_length= 50)
 	weight = models.CharField(max_length= 50)
 
+	
 	def __str__(self): 
 		return self.shipment_id
 
