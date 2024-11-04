762f4355698efc6eb8dda5d3d8e2bdd540d978af
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-11-04 12:45:47 +0000

    consolidated templates in website folder

--- app/settings.py
+++ app/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
     'dash_board',
+
 ]
 
 MIDDLEWARE = [
---
+++ dash_board/resources.py
@@ -0,0 +1,25 @@
+from import_export import resources, fields
+from dash_board.models import Shipment
+from import_export.widgets import ForeignKeyWidget
+
+class dash_boardResource(resources.ModelResource):
+	category= fields.Field(
+		column_name='category',
+		attribute='category',
+		widget=ForeignKeyWidget(Category, field='name'))
+
+    class Meta:
+        model = dash_board
+        fields =(
+        	'shipment_id',
+        	'consignee',
+        	'ata',        	
+        	'cargo_available',
+        	'date_cleared',
+        	'actual_delivery',
+        	'cont',
+        	'twenty_ft',
+        	'forty_ft',
+        	'uw',
+        	'weight',
+        	) 
