1dd791c24a98ebec93b239efb832e107b0420a3d
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-11-07 19:56:59 +0000

    fixed no attribute for login_user

--- dash_board/urls.py
+++ dash_board/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path('website/tables', views.tables, name= "tables"),
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
-    path('login/', views.register_user, name="register"),
+    path('register/', views.register_user, name="register"),
 ]  
