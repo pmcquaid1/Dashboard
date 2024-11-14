c8f096fce2ce3ba9458749a8528ae0dfb712cc8f
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-11-14 13:52:17 +0000

    created homepage1 with business welcome page template

--- dash_board/forms.py
+++ dash_board/forms.py
@@ -1,7 +1,66 @@
 from django import forms
 from .models import Shipment
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, SetPasswordForm
+from django.contrib.auth.models import User 
 
 class ShipmentForm(forms.ModelForm):
 		class Meta:
 			model = Shipment
 			fields = ["shipment_id", "consignee", "ata", "cargo_available", "date_cleared", "actual_delivery", "cont", "twenty_ft", "forty_ft","uw", "weight",]
+
+
+class UpdateUserForm(UserChangeForm):
+	password = None
+	first_name = forms.CharField(label="", max_length= 100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}), required=False)
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}), required=False)
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=False)
+
+	class Meta:
+		model = User
+		fields =('username', 'first_name', 'last_name','email')
+
+def __init__(self, *args, **kwargs):
+	super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+	self.fields['username'].widget.attrs['class'] = 'form-control'
+	self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+	self.fields['username'].label = ''
+	self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
+
+class UpdateContact(forms.ModelForm):
+	first_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}), required=False)
+	last_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}), required=False)
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=False)
+
+#Register New User
+
+class SignUpForm(UserCreationForm):
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), required=False)
+	first_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}), required=False)
+	last_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}), required=False)
+
+	class Meta:
+		model = User
+		fields =('username', 'first_name', 'last_name','email','password1', 'password2')
+
+#https://github.com/flatplanet/Django-Ecommerce/blob/main/store/forms.py
+
+def __init__(self, *args, **kwargs):
+	super(SignUpForm, self).__init__(*args, **kwargs)
+
+	self.fields['username'].widget.attrs['class'] = 'form-control'
+	self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+	self.fields['username'].label = ''
+	self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
+	self.fields['password1'].widget.attrs['class'] = 'form-control'
+	self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+	self.fields['password1'].label = ''
+	self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+
+	self.fields['password2'].widget.attrs['class'] = 'form-control'
+	self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+	self.fields['password2'].label = ''
+	self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'  
+
--- dash_board/models.py
+++ dash_board/models.py
@@ -18,6 +18,16 @@ class Shipment(models.Model):
 	def __str__(self): 
 		return self.shipment_id
 
+class Contact(models.Model):
+	first_name = models.CharField(max_length= 50)
+	last_name = models.CharField(max_length= 50)
+	email = models.CharField(max_length= 50)
+	
+
+	
+	def __str__(self): 
+		return self.first_name
+
 
 
 
