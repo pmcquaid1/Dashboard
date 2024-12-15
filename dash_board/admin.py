66374d0b04f899b4e2c0b2aa3e75d3ca02ce0df0
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-12-15 18:39:04 +0000

    register bill on admin.py

--- dash_board/admin.py
+++ dash_board/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Shipment
 from .models import Transport
+from .models import Bill
+
 from import_export.admin import ImportExportModelAdmin
 
 
 @admin.register(Shipment)
 @admin.register(Transport)
+@admin.register(Bill)
 
 class ViewAdmin(ImportExportModelAdmin):
 	pass
