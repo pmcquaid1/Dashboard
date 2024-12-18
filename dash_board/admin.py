bce6f3080f389f97b3beb5db1a8971c71efdbdb9
Patrick McQuaid <patrick.mcquaid@stellar-africa.com>
2024-12-18 14:04:31 -0500

    added org form, bill2 form, bill2 html, org html

--- dash_board/admin.py
+++ dash_board/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from .models import Shipment
 from .models import Transport
 from .models import Bill
-from .models import Organizations
+from .models import Organization
 from import_export.admin import ImportExportModelAdmin
 
 
 @admin.register(Shipment)
 @admin.register(Transport)
 @admin.register(Bill)
+@admin.register(Organization)
 
 class ViewAdmin(ImportExportModelAdmin):
 	pass
