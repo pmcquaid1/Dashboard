ba19dec0aa72ba1975b0afb1c6dd06b9803ab409
Patrick Mcquaid <pmcquaid67@gmail.com>
2025-06-17 14:25:00 +0000

    fixing navbar issue and urls

--- app/settings.py
+++ app/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR.parent / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
