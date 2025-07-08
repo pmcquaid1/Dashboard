2fe99f00ae4c98d53d3659d2bb2180240ab48eb3
Patrick Mcquaid <pmcquaid67@gmail.com>
2025-07-08 19:04:16 -0400

    updated project urls.py to serve static files during development

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dash_board.urls')),  # ✅ This line connects your app's URLs
+    path('', include('dash_board.urls')),  # ✅ Connects your app's URLs
 ]
+
+# ✅ Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
