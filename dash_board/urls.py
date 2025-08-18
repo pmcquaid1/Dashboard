f5012aa5e77c3d11a6f37f3f6de72fc26424d8ee
Patrick Mcquaid <pmcquaid67@gmail.com>
2025-08-18 20:10:07 +0000

    correct comma syntax error in urls.py for healthcheck

--- dash_board/urls.py
+++ dash_board/urls.py
@@ -8,6 +8,7 @@ from .views import test_home
 
 urlpatterns = [
     path("", test_home, name="test_home"),
+
     # Core Pages
     path('', views.landing, name="landing"),
     path('base/', views.base, name="base"),
@@ -28,7 +29,8 @@ urlpatterns = [
     path('employee/', views.register, name='employee'),
     path('employee/confirmation/', TemplateView.as_view(template_name='employee_confirmation.html'), name='employee_confirmation'),
     path('ping/', views.ping),
-    path('healthcheck/', lambda r: HttpResponse("OK"), name='healthcheck')
+    path('healthcheck/', lambda r: HttpResponse("OK"), name='healthcheck'),
+
     # Shipments
     path('shipment_test/', views.shipment_test, name="shipment_test"),
     path('document_test/', views.document_test, name="document_test"),
