0d32ca7bd66e1818c1d6ed0119e78c265cafe16b
Patrick Mcquaid <pmcquaid67@gmail.com>
2025-09-02 07:11:10 +0000

    updated test_urls.py

--- dash_board/test_urls.py
+++ dash_board/test_urls.py
@@ -10,24 +10,9 @@ urlpatterns = [
     path('test-template/', test_views.test_template_render),
 
     # API endpoints with audit logging and dry-run support
-    path(
-        'api/test/ping',
-        log_test_access(dry_run_safe(ping_view)),
-        name='ping'
-    ),
-    path(
-        'api/test/shipment/',
-        log_test_access(dry_run_safe(test_views.receive_shipment_xml)),
-        name='receive_shipment_xml'
-    ),
-    path(
-        'api/test/document/',
-        log_test_access(dry_run_safe(test_views.receive_document_xml)),
-        name='receive_document_xml'
-    ),
-    path(
-        'api/test/status/',
-        log_test_access(dry_run_safe(test_views.status_view)),
-        name='status_view'
-    ),
+    path('api/test/ping', log_test_access(dry_run_safe(ping_view)), name='ping'),
+    path('api/test/shipment/', log_test_access(dry_run_safe(test_views.receive_shipmentxml)), name='receive_shipmentxml'),
+    path('api/test/document/', log_test_access(dry_run_safe(test_views.receive_documentxml)), name='receive_documentxml'),
+    path('api/test/status/', log_test_access(dry_run_safe(test_views.status_view)), name='status_view'),
 ]
+
